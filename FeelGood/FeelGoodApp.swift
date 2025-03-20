33f79a4eb5c7ee15f996328be79f397bfcdacdcd
Ayush Shah <aas225@lehigh.edu>
2025-03-20 14:38:56 -0500

    Initial Commit

---
+++ FeelGood/FeelGoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeelGoodApp.swift
+//  FeelGood
+//
+//  Created by Ayush Shah on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct FeelGoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
